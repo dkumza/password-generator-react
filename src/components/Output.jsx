d3e269eb7903c70e1f7c6094682712f90dad5af4
Darius <dariuskumza@gmail.com>
2023-11-24 19:01:22 +0200

    symbols and numbers styling

--- src/components/Output.jsx
+++ src/components/Output.jsx
@@ -3,34 +3,47 @@ import { useState, useEffect } from "react";
 import { abc, numbers, symbols } from "../data";
 
 export default function Output({ length, numbersSelected, symbolsSelected }) {
-   const [newPassw, setNewPassw] = useState("");
+   const [newPassw, setNewPassw] = useState([]);
 
    useEffect(() => {
       if (length < 8 || length > 100) return;
       let newPasswArray = [];
-      const randomAbc = () => abc[Math.floor(Math.random() * abc.length)];
-      const randomNumber = () =>
-         numbers[Math.floor(Math.random() * numbers.length)];
-      const randomSymbol = () =>
-         symbols[Math.floor(Math.random() * symbols.length)];
+      const randomAbc = () => ({
+         char: abc[Math.floor(Math.random() * abc.length)],
+         type: "",
+      });
+      const randomNumber = () => ({
+         char: numbers[Math.floor(Math.random() * numbers.length)],
+         type: "text-sky-600",
+      });
+      const randomSymbol = () => ({
+         char: symbols[Math.floor(Math.random() * symbols.length)],
+         type: "text-rose-600",
+      });
 
       const makeRandomPassw = (length) => {
          for (let i = 0; i < length; i++) {
-            if (newPasswArray.length < length) newPasswArray.push(randomAbc());
+            if (newPasswArray.length < length) {
+               newPasswArray.push(randomAbc());
+            }
             if (newPasswArray.length < length && numbersSelected)
                newPasswArray.push(randomNumber());
             if (newPasswArray.length < length && symbolsSelected)
                newPasswArray.push(randomSymbol());
          }
-         setNewPassw(newPasswArray.join(""));
+         setNewPassw(newPasswArray);
       };
 
       makeRandomPassw(length);
    }, [length, numbersSelected, symbolsSelected]);
 
    return (
-      <div className="border mt-1 text-center text-xl py-2 px-4 max-w-full break-all">
-         {newPassw}
+      <div className="border mt-1 text-center text-2xl tracking-wider font-semibold py-2 px-4 max-w-full break-all ">
+         {newPassw.map((item, index) => (
+            <span key={index} className={item.type}>
+               {item.char}
+            </span>
+         ))}
       </div>
    );
 }
