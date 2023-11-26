b67b591146e4d24df7e2e80db578ecf8688287d4
Darius <dariuskumza@gmail.com>
2023-11-26 16:40:25 +0200

    localstorage fixes

--- src/components/History.jsx
+++ src/components/History.jsx
@@ -3,18 +3,26 @@ import { useState, useEffect } from "react";
 
 export const History = ({ allPasswords }) => {
    // state to save all new passwords
-   const [savedPassw, setSavedPassw] = useState([]);
+   const [savedPassw, setSavedPassw] = useState(
+      // check if localStorage exists, if so show last 10 passwords (on refresh there will be last value new password)
+      // else returns empty string
+      JSON.parse(localStorage.getItem("data")) || []
+   );
 
    useEffect(() => {
-      // push to back of array new generated password
-      setSavedPassw((savedPassw) => [allPasswords, ...savedPassw]);
+      // if allpasswords are empty return
+      if (allPasswords.length === 0) return;
+      // push to back of array new generated password and cut 12 values from start
+      let newPassword = [allPasswords, ...savedPassw].splice(0, 10);
+      localStorage.setItem("data", JSON.stringify(newPassword));
+      setSavedPassw(newPassword);
    }, [allPasswords]);
 
    return (
       <div className="w-3/4 mt-4">
          <h1 className="text-lg font-semibold">Last 10 generated passwords:</h1>
          <p className="flex flex-col text-center w-full">
-            {savedPassw.slice(0, 10).map((item, index) => (
+            {savedPassw.map((item, index) => (
                <li
                   className="w-full list-none truncate text-sm
                "
--- src/components/Output.jsx
+++ src/components/Output.jsx
@@ -52,18 +52,17 @@ export default function Output({
       const newPassword = makeRandomPassw(length);
       setNewPassw(newPassword);
       if (length >= 8) strongVal = strongLevel[0];
-      if ((length >= 8 && numbersSelected) || (length >= 8 && symbolsSelected))
-         strongVal = strongLevel[1];
       if (length >= 12) strongVal = strongLevel[1];
+      if (length >= 14) strongVal = strongLevel[2];
+      if (length >= 16) strongVal = strongLevel[3];
+      if (
+         (length >= 10 && numbersSelected) ||
+         (length >= 10 && symbolsSelected)
+      )
+         strongVal = strongLevel[1];
       if (
          (length >= 12 && numbersSelected) ||
          (length >= 12 && symbolsSelected)
-      )
-         strongVal = strongLevel[2];
-      if (length >= 16) strongVal = strongLevel[2];
-      if (
-         (length >= 16 && numbersSelected) ||
-         (length >= 16 && symbolsSelected)
       )
          strongVal = strongLevel[3];
 
@@ -74,7 +73,7 @@ export default function Output({
    }, [length, numbersSelected, symbolsSelected, clickCount]);
 
    return (
-      <div className="flex justify-between border rounded-3xl border-sky-500 mt-1 text-center text-2xl tracking-wider  font-semibold py-2 px-4 max-w-full  shadow bg-white w-3/4 truncate">
+      <div className="flex leading-normal justify-between items-center border rounded-3xl border-sky-500 mt-1 text-center text-2xl tracking-wider  font-semibold py-2 px-4 max-w-full  shadow bg-white w-3/4 truncate">
          <div className="passw-wrap ml-2 truncate">
             {newPassw.map((item, index) => (
                <span key={index} className={item.type}>
@@ -82,19 +81,19 @@ export default function Output({
                </span>
             ))}
          </div>
-         <div className="left-wrap flex">
-            <div className="">
+         <div className="flex items-center">
+            <div className="flex">
                <span
                   className={[
                      strong.color,
-                     "mr-2 px-3 py-1 rounded-lg text-sm align-middle  text-white",
+                     "mr-2 px-3 py-1 rounded-lg text-sm text-white",
                   ].join(" ")}
                >
                   {strong.id}
                </span>
             </div>
             <button
-               className=" mr-2"
+               className="mr-2 "
                onClick={() => {
                   setNewPassw(makeRandomPassw(length));
                   setClickCount((prevCount) => prevCount + 1);
